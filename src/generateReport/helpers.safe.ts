62917c659d2706e7a6d7b05694e82752b0de2fb4
David Colon <38386583+Da-Colon@users.noreply.github.com>
2025-02-19 09:48:23 -0500

    Enhance getSafeData function to retrieve multisig transactions and count unique users and votes; improve transaction handling

--- src/generateReport/helpers.safe.ts
+++ src/generateReport/helpers.safe.ts
@@ -1,6 +1,6 @@
 import type SafeApiKit from "@safe-global/api-kit";
 import { identifyContract } from "./helpers.contract";
-import type { Address, PublicClient } from "viem";
+import { getAddress, type Address, type Hex, type PublicClient } from "viem";
 import type { ContractType } from "./types.contract";
 
 export async function getSafeData(
@@ -10,6 +10,34 @@ export async function getSafeData(
 ) {
   const safeInfo = await safeClient.getSafeInfo(daoAddress);
   const safeCreationInfo = await safeClient.getSafeCreationInfo(daoAddress);
+  const multisigTransactions = await safeClient.getMultisigTransactions(daoAddress);
+  const pendingTransactions = await safeClient.getPendingTransactions(daoAddress);
+  // combine and filter out any duplicate transactions
+  const allTransactions = [...multisigTransactions.results, ...pendingTransactions.results].filter(
+    (tx, index, self) => self.findIndex((t) => t.transactionHash === tx.transactionHash) === index,
+  );
+
+  const allProposals = allTransactions.map((tx, index) => {
+    return {
+      proposalId: BigInt(index + 1),
+      proposer: getAddress(tx.proposer as Address),
+    };
+  });
+
+  const allUniqueUsers = new Set<Address>();
+  let votesCount = 0;
+  for (const proposal of allTransactions) {
+    if (proposal?.proposer) {
+      allUniqueUsers.add(proposal.proposer);
+    }
+    if (proposal?.confirmations) {
+      for (const confirmation of proposal.confirmations) {
+        allUniqueUsers.add(getAddress(confirmation.owner));
+        votesCount += 1;
+      }
+    }
+  }
+
   const modules = safeInfo.modules;
   const owners = safeInfo.owners;
   const guard = safeInfo.guard;
@@ -26,8 +54,12 @@ export async function getSafeData(
   }
   return {
     timeOfSafeCreation: safeCreationInfo.created,
+    deploymentTransactionHash: safeCreationInfo.transactionHash as Hex,
     owners,
     guard,
     modules: decentModules,
+    multisigTransactions: allProposals,
+    uniqueMultisigUsers: Array.from(allUniqueUsers),
+    multisigVotesCount: votesCount,
   };
 }
